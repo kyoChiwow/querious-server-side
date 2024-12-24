429a12e56454d4ebbb239ec56aff3fd5fb192ded
kyoChiwow <kyoayasakicsgo@gmail.com>
2024-12-24 10:47:52 +0600

    Done with the jwt token thing, I also added the verify token and stored the token in the cookies

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // Middleware Settings here
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  }));
 app.use(express.json());
+app.use(cookieParser());
 // Middleware Settings here
+
+// Verify Token here
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+
+  if(!token) {
+    return res
+    .status(401)
+    .send({message: "You are not supposed to be here!"})
+  }
+
+  jwt.verify(token, process.env.ACCESS_SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Access Denied"});
+    }
+    req.user = decoded;
+    next();
+  })
+}
+// Verify Token here
 
 // MongoDB Settings here
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.parzq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -29,6 +56,34 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    // Creating Product Collection here
+    const productCollection = client.db("queryProducts").collection("products");
+    // Creating Product Collection here
+
+    // JWT APIS
+    app.post("/jwt", async(req,res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_SECRET_KEY, {
+        expiresIn: "24h",
+      })
+      res
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: false,
+      })
+      .send({ success: true });
+    })
+
+    app.post("/logout", (req, res) => {
+      res
+      .clearCookie("token", {
+        httpOnly: true,
+        secure: false,
+      })
+      .send({ success: true });
+    })
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
