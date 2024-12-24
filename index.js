40165b4406238d9d3e5aa897b6fccf7779b9584d
kyoChiwow <kyoayasakicsgo@gmail.com>
2024-12-24 19:37:50 +0600

    Made a little bit of adjustments

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // Middleware Settings here
 app.use(
   cors({
     origin: ["http://localhost:5173"],
     credentials: true,
-  }));
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 // Middleware Settings here
@@ -21,20 +22,20 @@ app.use(cookieParser());
 const verifyToken = (req, res, next) => {
   const token = req.cookies?.token;
 
-  if(!token) {
+  if (!token) {
     return res
-    .status(401)
-    .send({message: "You are not supposed to be here!"})
+      .status(401)
+      .send({ message: "You are not supposed to be here!" });
   }
 
   jwt.verify(token, process.env.ACCESS_SECRET_KEY, (err, decoded) => {
     if (err) {
-      return res.status(401).send({ message: "Access Denied"});
+      return res.status(401).send({ message: "Access Denied" });
     }
     req.user = decoded;
     next();
-  })
-}
+  });
+};
 // Verify Token here
 
 // MongoDB Settings here
@@ -46,7 +47,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -55,35 +56,36 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
 
     // Creating Product Collection here
-    const productCollection = client.db("queryProducts").collection("products");
+    const queryCollection = client.db("queryProducts").collection("products");
     // Creating Product Collection here
 
     // JWT APIS
-    app.post("/jwt", async(req,res) => {
+    app.post("/jwt", verifyToken, async (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_SECRET_KEY, {
         expiresIn: "24h",
-      })
+      });
       res
-      .cookie("token", token, {
-        httpOnly: true,
-        secure: false,
-      })
-      .send({ success: true });
-    })
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
 
     app.post("/logout", (req, res) => {
       res
-      .clearCookie("token", {
-        httpOnly: true,
-        secure: false,
-      })
-      .send({ success: true });
-    })
-
+        .clearCookie("token", {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -94,11 +96,10 @@ run().catch(console.dir);
 
 // Get and Listen Settings here
 app.get("/", (req, res) => {
-    res.send("Product server is running")
-})
+  res.send("Product server is running");
+});
 
 app.listen(port, () => {
-    console.log(`Product server is running on PORT: ${port}`);
-})
+  console.log(`Product server is running on PORT: ${port}`);
+});
 // Get and Listen Settings here
-
