60a5bbf69e68ea9c2d18bc7ac7c54a12e81b9c63
kyoChiwow <kyoayasakicsgo@gmail.com>
2024-12-26 18:22:32 +0600

    Done with the recommendme api

--- index.js
+++ index.js
@@ -153,21 +153,21 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/products", async (req, res) => {
+    app.get("/products",verifyToken, async (req, res) => {
       const email = req.query.email;
       const query = { recommenderEmail: email };
       const result = await recommendedCollection.find(query).toArray();
       res.send(result);
     });
 
-    app.delete("/products/:id", async (req, res) => {
+    app.delete("/products/:id",verifyToken, async (req, res) => {
       const recommendId = req.params.id;
       const recommendQuery = { _id: new ObjectId(recommendId) };
       const result = await recommendedCollection.deleteOne(recommendQuery);
       res.send(result);
     });
 
-    app.patch("/products/:id", async (req, res) => {
+    app.patch("/products/:id",verifyToken, async (req, res) => {
       const queryId = req.params.id;
       const productQuery = { _id: new ObjectId(queryId) };
       const updateQuery = {
@@ -176,6 +176,19 @@ async function run() {
       const result = await queryCollection.updateOne(productQuery, updateQuery);
       res.send(result);
     });
+
+    app.get("/recommendme", verifyToken, async (req, res) => {
+      const email = req.query.email;
+      const emailQueries = await queryCollection.find({ userEmail: email}).toArray();
+
+      const emailQueryIds = emailQueries.map(query => query._id.toString());
+
+      const getRecommendations = await recommendedCollection.find({
+        queryId: { $in: emailQueryIds }
+      }).toArray();
+
+      res.send(getRecommendations)
+    })
     // Recommended related APIS
   } finally {
     // Ensures that the client will close when you finish/error
