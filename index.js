ce62c32bce4135db02ff116add01a91103bb1962
kyoChiwow <kyoayasakicsgo@gmail.com>
2024-12-26 22:10:21 +0600

    ajusting

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
-require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
